52c5e8a04a1e60ef22403080c4f1ad1ecfb1e342
pikaleva_natalya <deva032006@yandex.ru>
2025-03-07 17:24:44 +0000

    Вариант синхронный с использованием flask

---
+++ main/utils.py
@@ -0,0 +1,6 @@
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    """Проверяет, является ли расширение файла допустимым."""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
